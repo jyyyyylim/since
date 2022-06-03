fed749b74e363c02ab9a95009a400d99860614d9
俊。 <junyaolim0206@gmail.com>
2022-06-03 12:14:05 +0800

    Update since.go

--- since.go
+++ since.go
@@ -10,20 +10,45 @@ import (
 //intended: 24h format
 
 func main() {
-	dh, dm, init := format(os.Args[1])
+
+	var dh, dm int
+
+	if len(os.Args) > 2 {
+		dh, dm = tformat(os.Args[1]+" "+os.Args[2], int16(len(os.Args)))
+	} else {
+		dh, dm = tformat(os.Args[1], int16(len(os.Args)))
+	}
 	//prints to stdout
-	fmt.Println(diff(dh, dm, init).Format("15h 04m"))
+
+	fmt.Println(diff(dh, dm).Format("15h 04m"))
+
+	return
 }
 
-func format(startingtime string) (int, int, time.Time) {
+
+
+func tformat(startingtime string, arglen int16) (int, int) {
+
 	//why wont golang just adopt strftime?
-	const layout string = "15:04"
+	var layout string
+
+	if arglen > 2 {
+		layout = "15 04"
+	} else {
+		layout = "15:04"
+	}
+
 	fstarting, _ := time.Parse(layout, startingtime)
 	dh, dm, _ := fstarting.Clock()
-	return dh, dm, fstarting
+
+	return dh, dm
 }
 
-func diff(dh, dm int, fstarting time.Time) time.Time {
+
+
+func diff(dh, dm int) time.Time {
+
 	elapsed := time.Now().Add(-time.Minute * time.Duration(dm+(dh*60)))
+
 	return elapsed
 }
